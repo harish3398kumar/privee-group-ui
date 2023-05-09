e8d99bfca3e3a4e81dd800d0f83c97b0c6d10e3c
harish3398kumar <harishkumar.softsuave@gmail.com>
2023-05-09 15:51:58 +0530

    added basic header and footer

--- privee-group-ui/src/app/app.module.ts
+++ privee-group-ui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
